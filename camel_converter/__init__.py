8664a8dfadc3431cdaf0eeea0fe6e4a196ab87c7
bjeffrey92 <bjeffrey92@hotmail.com>
2023-10-26 09:55:24 +0100

    optionally treat digits as capitals when converting to snake case

--- camel_converter/__init__.py
+++ camel_converter/__init__.py
@@ -111,16 +111,26 @@ def to_camel(snake_string: str) -> str:
 
 
 @lru_cache(maxsize=4096)
-def to_snake(camel_string: str) -> str:
+def to_snake(camel_string: str, treat_digits_as_capitals: bool = False) -> str:
     """Converts a camel case or pascal case string to snake case.
 
     Args:
-        camel_string: String in camel case or pascal case format. For example myVariable.
+        camel_string:
+            String in camel case or pascal case format. For example myVariable.
+        treat_digits_as_capitals:
+            Whether to treat digits as capitals. For example myVariable2 would become my_variable_2 rather than my_variable2.
 
     Returns:
         The string in snake case format. For example my_variable.
     """
-    return "".join([f"_{c}" if c.isupper() else c for c in camel_string]).lstrip("_").lower()
+
+    def check_character(c: str) -> str:
+        if c.isupper() or (treat_digits_as_capitals and c.isdigit()):
+            return f"_{c}"
+        else:
+            return c
+
+    return "".join([check_character(c) for c in camel_string]).lstrip("_").lower()
 
 
 @lru_cache(maxsize=4096)
